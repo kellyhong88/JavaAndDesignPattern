e8c696ef38ccab17fe5ad3c52e5e1e69898bf06b
xijun.lh <xijun.lh@antfin.com>
2020-08-22 18:52:27 +0800

    update insertion sort

--- src/main/java/com/learning/java/algorithm/sort/BubbleSort.java
+++ src/main/java/com/learning/java/algorithm/sort/BubbleSort.java
@@ -21,11 +21,11 @@ public class BubbleSort {
     static void sort(int[] array) {
         if (ArrayUtils.noNeedSort(array)) return;
 
-        int i, j, temp;
+        int temp;
         boolean isChange = false;
 
-        for (i = 0; i < array.length - 1; i++) {
-            for (j = 0; j < array.length - 1 - i; j++) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
                 if (array[j] > array[j + 1]) {
                     temp = array[j];
                     array[j] = array[j + 1];
--- src/main/java/com/learning/java/algorithm/sort/InsertionSort.java
+++ src/main/java/com/learning/java/algorithm/sort/InsertionSort.java
@@ -1,4 +1,25 @@
 package com.learning.java.algorithm.sort;
 
 public class InsertionSort {
+
+    public static void sort(int[] array) {
+        if (ArrayUtils.noNeedSort(array)) return;
+
+        int temp;
+        for (int i = 1; i < array.length; i++) {
+            while (i >= 1 && array[i - 1] > array[i]) {
+                temp = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = temp;
+                i--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayUtils.print();
+        InsertionSort.sort(ArrayUtils.array);
+        ArrayUtils.print();
+    }
+
 }
