0e40dafb27478dd686806ad4e2a0ae8140ceb417
xijun.lh <xijun.lh@antfin.com>
2020-09-01 16:42:27 +0800

    determine whether it is a binary search tree

--- src/main/java/com/learning/java/algorithm/tree/BinarySearchTree.java
+++ src/main/java/com/learning/java/algorithm/tree/BinarySearchTree.java
@@ -2,8 +2,16 @@ package com.learning.java.algorithm.tree;
 
 public class BinarySearchTree extends BinaryTree {
 
+    public static boolean isValidBinarySearchTree(Node root, Node lower, Node upper) {
+        if (root == null) return true;
+        if (lower != null && root.data <= lower.data) return false;
+        if (upper != null && root.data >= upper.data) return false;
+        return isValidBinarySearchTree(root.left, null, root) && isValidBinarySearchTree(root.right, root, null);
+    }
+
     public static void main(String[] args) {
         Node root = BinaryTree.createBinarySearchTree();
+        System.out.println(isValidBinarySearchTree(root, null, null));
         BinaryTree.printInOrder(root);
     }
 
