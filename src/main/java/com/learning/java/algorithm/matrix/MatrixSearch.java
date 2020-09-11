c6e6894f850e5b37a6a2475c842b4651e2c21153
xijun.lh <xijun.lh@antfin.com>
2020-09-11 16:11:01 +0800

    matrix search

--- src/main/java/com/learning/java/algorithm/matrix/MatrixSearch.java
+++ src/main/java/com/learning/java/algorithm/matrix/MatrixSearch.java
@@ -2,9 +2,14 @@ package com.learning.java.algorithm.matrix;
 
 public class MatrixSearch {
 
-    public static int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+    public static int[][] MatrixA = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+    public static int[][] MatrixB = {
+            {1, 4, 7, 11, 15},
+            {2, 5, 8, 12, 19},
+            {3, 6, 9, 16, 22}
+    };
 
-    public static boolean existed(int[][] matrix, int target) {
+    public static boolean existedInMatrixA(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
 
         int m = matrix.length, n = matrix[0].length;
@@ -20,11 +25,10 @@ public class MatrixSearch {
                 left = pivotIdx + 1;
             }
         }
-
         return false;
     }
 
-    public static boolean existed2(int[][] matrix, int target) {
+    public static boolean existedInMatrixA2(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
 
         int row = 0, column = matrix[0].length - 1;
@@ -39,11 +43,31 @@ public class MatrixSearch {
         return false;
     }
 
+    public static boolean existedInMatrixB(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+
+        int row = matrix.length - 1, column = 0;
+        while (row >= 0 && column < matrix[0].length) {
+            if (matrix[row][column] < target) {
+                column++;
+            } else if (matrix[row][column] > target) {
+                row--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Does 3 exist in the matrix: " + existed(matrix, 3));
-        System.out.println("Does 13 exist in the matrix: " + existed(matrix, 13));
-        System.out.println("Does 3 exist in the matrix: " + existed2(matrix, 3));
-        System.out.println("Does 13 exist in the matrix: " + existed2(matrix, 13));
+        System.out.println("Does 3 exist in the MatrixA: " + existedInMatrixA(MatrixA, 3));
+        System.out.println("Does 13 exist in the MatrixA: " + existedInMatrixA(MatrixA, 13));
+        System.out.println("Does 3 exist in the MatrixA: " + existedInMatrixA2(MatrixA, 3));
+        System.out.println("Does 13 exist in the MatrixA: " + existedInMatrixA2(MatrixA, 13));
+        System.out.println("Does 8 exist in the MatrixB: " + existedInMatrixB(MatrixB, 8));
+        System.out.println("Does 17 exist in the MatrixB: " + existedInMatrixB(MatrixB, 17));
+        System.out.println("Does 19 exist in the MatrixB: " + existedInMatrixB(MatrixB, 19));
+        System.out.println("Does 24 exist in the MatrixB: " + existedInMatrixB(MatrixB, 24));
     }
 
 }
