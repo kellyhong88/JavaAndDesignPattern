178f8a327124ceb0b2a56ee31f9e921bdfd24533
xijun.lh <xijun.lh@antfin.com>
2020-09-21 21:14:30 +0800

    calculate rain water

--- src/main/java/com/learning/java/algorithm/array/RainWater.java
+++ src/main/java/com/learning/java/algorithm/array/RainWater.java
@@ -5,10 +5,26 @@ import static com.learning.java.algorithm.sort.ArrayUtils.*;
 public class RainWater {
 
     public static int calculateRainWater(int[] array) {
-        if (emptyArray(array)) return 0;
+        if (emptyArray(array) || array.length == 1) return 0;
 
+        int i, rainWater = 0;
+        int[] leftArray = new int[array.length], rightArray = new int[array.length];
+        leftArray[0] = array[0];
+        rightArray[array.length - 1] = array[array.length - 1];
 
-        return 0;
+        for (i = 1; i < array.length; i++) {
+            leftArray[i] = Math.max(leftArray[i - 1], array[i]);
+        }
+
+        for (i = array.length - 2; i >= 0; i--) {
+            rightArray[i] = Math.max(rightArray[i + 1], array[i]);
+        }
+
+        for (i = 0; i < array.length; i++) {
+            rainWater += (Math.min(leftArray[i], rightArray[i]) - array[i]);
+        }
+
+        return rainWater;
     }
 
     public static void main(String[] args) {
