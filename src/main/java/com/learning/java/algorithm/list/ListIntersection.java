85d440d3e9ddad7f47f5b27126826d63f4b08909
kellyhong88 <kellyhong88@163.com>
2020-12-06 21:21:19 +0800

    add explanations of intersection list

--- src/main/java/com/learning/java/algorithm/list/ListIntersection.java
+++ src/main/java/com/learning/java/algorithm/list/ListIntersection.java
@@ -1,10 +1,18 @@
 package com.learning.java.algorithm.list;
 
+/**
+ * 两条list，在某个节点处交汇为1条list
+ * a1->a2->c1->c2->c3 如：4->1->8->7->5
+ * b1->b2->b3->c1->c2->c3 如：3->6->2->8->7->5
+ * 其中：c1->c2->c3（即：8->7->5）为共有节点
+ * 如何获取交汇节点？
+ *
+ * */
 public class ListIntersection extends BaseList {
 
     public static Node findIntersectionNode(Node head1, Node head2) {
-        if (head1 == null) return null;
-        if (head2 == null) return null;
+        if (head1 == null || head2 == null) return null;
+        if (head1 == head2) return head1;
 
         Node p1 = head1;
         Node p2 = head2;
@@ -26,37 +34,41 @@ public class ListIntersection extends BaseList {
     }
 
     public static Node findIntersectionNode2(Node head1, Node head2) {
-        if (head1 == null) return null;
-        if (head2 == null) return null;
+        if (head1 == null || head2 == null) return null;
+        if (head1 == head2) return head1;
 
         Node p1 = head1;
         Node p2 = head2;
-        int length1 = 0, length2 = 0;
-        while (p1 != null) {
+        int length1 = 1, length2 = 1;
+        while (p1.next != null) {
             length1++;
             p1 = p1.next;
         }
-        while (p2 != null) {
+        while (p2.next != null) {
             length2++;
             p2 = p2.next;
         }
+
+        if (p1 != p2) return null;
 
         p1 = head1;
         p2 = head2;
         if (length1 > length2) {
             int diff = length1 - length2;
-            while (diff > 0) {
+            while (diff > 0 && p1 != null) {
                 p1 = p1.next;
                 diff--;
             }
         }
         if (length2 > length1) {
             int diff = length2 - length1;
-            while (diff > 0) {
+            while (diff > 0 && p2 != null) {
                 p2 = p2.next;
                 diff--;
             }
         }
+
+        if (p1 == p2) return p1;
 
         while (p1 != null && p2 != null && p1 != p2) {
             p1 = p1.next;
@@ -75,17 +87,17 @@ public class ListIntersection extends BaseList {
         temp2 = baseList.new Node(8);
         temp1.next = temp2;
         temp1 = temp2;
-        temp2 = baseList.new Node(4);
+        temp2 = baseList.new Node(7);
         temp1.next = temp2;
         temp1 = temp2;
         temp2 = baseList.new Node(5);
         temp1.next = temp2;
 
-        head2 = temp1 = baseList.new Node(5);
+        head2 = temp1 = baseList.new Node(3);
         temp2 = baseList.new Node(6);
         temp1.next = temp2;
         temp1 = temp2;
-        temp2 = baseList.new Node(1);
+        temp2 = baseList.new Node(2);
         temp1.next = temp2;
         temp2.next = head1.next.next;
 
