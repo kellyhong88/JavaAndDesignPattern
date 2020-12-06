13ced8a2d6ebf0b18d897a8f1950f4fc9ce5c88b
kellyhong88 <kellyhong88@163.com>
2020-12-06 22:12:27 +0800

    determine whether a tree is a binary search tree

--- src/main/java/com/learning/java/algorithm/tree/BinarySearchTree.java
+++ src/main/java/com/learning/java/algorithm/tree/BinarySearchTree.java
@@ -1,7 +1,13 @@
 package com.learning.java.algorithm.tree;
 
+/**
+ * 二叉搜索/查找树
+ * */
 public class BinarySearchTree extends BinaryTree {
 
+    /**
+     * 如何判断是否是二叉搜索树？
+     * */
     public static boolean isValidBinarySearchTree(Node root, Node lower, Node upper) {
         if (root == null) return true;
         if (lower != null && root.data <= lower.data) return false;
