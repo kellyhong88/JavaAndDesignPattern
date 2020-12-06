116a5f23f695b6a670489543b454383c57c49ed2
kellyhong88 <kellyhong88@163.com>
2020-12-06 20:32:55 +0800

    add explanations of cycled list

--- src/main/java/com/learning/java/algorithm/list/ListCycle.java
+++ src/main/java/com/learning/java/algorithm/list/ListCycle.java
@@ -1,5 +1,26 @@
 package com.learning.java.algorithm.list;
 
+/**
+ * 有环list：5->3->7->2->6->4->1->2->...
+ * 1.如何判断list中是否有环？
+ * 运用数学中的追及问题：环形跑道上两个运动员，跑得快的人势必会追上跑得慢的人，第n次追上是超前n圈
+ * 因此，两个指针，p1每次前进1个节点，p2每次前进2个节点，若list有环，则两个指针迟早会相遇
+ * 2.如何求出有环list的环长？
+ * 两个指针首次相遇，证明list有环
+ * 两个指针从相遇点继续循环前进，直到两个指针第二次相遇，p2比p1又多走了一圈
+ * p2对p1的速度差是1步，因此 环长 = 1步速度差 * 前进次数
+ * 注：相遇点未必是入环节点
+ * 3.如何求出有环list的入环节点？
+ * 首次相遇点必定在环上的某个节点处
+ * 假设list头节点到入环点的距离是D，入环点到首次相遇点的距离是S1，首次相遇点回到入环点的距离是S2
+ * 当p1、p2首次相遇时，p1走的距离是D+S1，p2走的距离是D+S1+S2+S1=D+2S1+S2
+ * p2的速度是p1的2倍，因此首次相遇时，p2走的距离也是p1的2倍
+ * 则：D+2S1+S2 = 2(D+S1)
+ * 得：D = S2
+ * 即：头节点到入环点的距离 = 首次相遇点到入环点的距离
+ * 若1个指针从头节点开始走，另1个指针从首次相遇点开始走，每次都向前1步，
+ * 则两个指针相遇的节点，就是入环节点
+ * */
 public class ListCycle extends BaseList {
 
     static boolean isCycle(Node head) {
@@ -11,6 +32,21 @@ public class ListCycle extends BaseList {
             if (p1 == p2) return true;
         }
         return false;
+    }
+
+    static int getCycleLength(Node head) {
+        Node firstMeet = firstMeet(head);
+        if (firstMeet == null) return 0;
+        Node p1 = firstMeet;
+        Node p2 = firstMeet;
+        int len = 0;
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+            len++;
+            if (p1 == p2) break;
+        }
+        return len;
     }
 
     static Node firstMeet(Node head) {
@@ -44,6 +80,7 @@ public class ListCycle extends BaseList {
         Node head2 = createCycledList();
         print(head2, cycleEntry(head2));
         System.out.println("whether this link is a cycled list: " + isCycle(head2));
+        System.out.println("the cycle length of this cycled list is " + getCycleLength(head2));
     }
 
 }
