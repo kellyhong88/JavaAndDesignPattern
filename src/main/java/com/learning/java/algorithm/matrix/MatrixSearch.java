10d5da32877d80b7993e0b941c761f24650e3fc3
xijun.lh <xijun.lh@antfin.com>
2020-12-31 21:08:59 +0800

    optimize algorithm of matrix

--- src/main/java/com/learning/java/algorithm/matrix/MatrixSearch.java
+++ src/main/java/com/learning/java/algorithm/matrix/MatrixSearch.java
@@ -4,15 +4,21 @@ import static com.learning.java.algorithm.matrix.MatrixUtils.*;
 
 public class MatrixSearch {
 
+    /**
+     * MatrixA 特点：
+     * 1. 二维数组中的每行都是递增序列
+     * 2. 二维数组中的每列都是递增序列
+     * 3. 整个二维数组是递增序列
+     * */
     public static boolean existedInMatrixA(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        if (emptyMatrix(matrix)) return false;
 
-        int m = getRowLength(matrix), n = getColumnLength(matrix);
-        int left = 0, right = m * n - 1;
+        int ROW_LEN = getRowLength(matrix), COLUMN_LEN = getColumnLength(matrix);
+        int left = 0, right = ROW_LEN * COLUMN_LEN - 1;
         int pivotIdx, pivot;
         while (left <= right) {
             pivotIdx = (left + right) / 2;
-            pivot = matrix[pivotIdx / m][pivotIdx % m];
+            pivot = matrix[pivotIdx / COLUMN_LEN][pivotIdx % COLUMN_LEN];
             if (target == pivot) return true;
             else if (target < pivot) {
                 right = pivotIdx - 1;
@@ -24,10 +30,11 @@ public class MatrixSearch {
     }
 
     public static boolean existedInMatrixA2(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        if (emptyMatrix(matrix)) return false;
 
-        int row = 0, column = getColumnLength(matrix) - 1;
-        while (row < matrix.length && column >= 0) {
+        int ROW_LEN = getRowLength(matrix), COLUMN_LEN = getColumnLength(matrix);
+        int row = 0, column = COLUMN_LEN - 1;
+        while (row < ROW_LEN && column >= 0) {
             if (matrix[row][column] == target) {
                 return true;
             } else if (matrix[row][column] < target) {
@@ -39,11 +46,18 @@ public class MatrixSearch {
         return false;
     }
 
+    /**
+     * MatrixB 特点：
+     * 1. 二维数组中的每行都是递增序列
+     * 2. 二维数组中的每列都是递增序列
+     * 3. 整个二维数组不是递增序列
+     * */
     public static boolean existedInMatrixB(int[][] matrix, int target) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        if (emptyMatrix(matrix)) return false;
 
-        int row = getRowLength(matrix) - 1, column = 0;
-        while (row >= 0 && column < matrix[0].length) {
+        int ROW_LEN = getRowLength(matrix), COLUMN_LEN = getColumnLength(matrix);
+        int row = ROW_LEN - 1, column = 0;
+        while (row >= 0 && column < COLUMN_LEN) {
             if (matrix[row][column] < target) {
                 column++;
             } else if (matrix[row][column] > target) {
--- src/main/java/com/learning/java/algorithm/matrix/MatrixUtils.java
+++ src/main/java/com/learning/java/algorithm/matrix/MatrixUtils.java
@@ -5,7 +5,8 @@ public class MatrixUtils {
     public static int[][] MatrixA = {
             {1, 3, 5, 7},
             {10, 11, 16, 20},
-            {23, 30, 34, 50}};
+            {23, 30, 34, 50}
+    };
 
     public static int[][] MatrixB = {
             {1, 4, 7, 11, 15},
