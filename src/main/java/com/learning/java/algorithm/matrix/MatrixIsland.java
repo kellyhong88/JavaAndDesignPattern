84438986490f9c843386dba0f5178d2f87be2052
xijun.lh <xijun.lh@antfin.com>
2021-01-20 16:54:17 +0800

    optimize calculating of islands number in matrix using DFS

--- src/main/java/com/learning/java/algorithm/matrix/MatrixIsland.java
+++ src/main/java/com/learning/java/algorithm/matrix/MatrixIsland.java
@@ -9,14 +9,31 @@ import static com.learning.java.algorithm.matrix.MatrixUtils.*;
  * */
 public class MatrixIsland {
 
+    /**
+     * 只有1和0两种元素的矩阵/二维数组
+     * 1：岛域
+     * 0：水域
+     * 一片连成片的1 + 四周都是0 -> 构成一个孤岛
+     * 计算矩阵中孤岛的个数
+     * */
     public static int calculateIslands(int[][] matrix) {
         if (emptyMatrix(matrix)) return 0;
 
         int count = 0;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
+                /**
+                 * 遇到1则开始遍历/探索当前的孤岛域
+                 * 遍历完则孤岛数count加1
+                 * */
                 if (matrix[i][j] == 1) {
+                    /**
+                     * 用深度优先遍历DFS来遍历出孤岛的范围
+                     * */
                     dfs(matrix, i, j);
+                    /**
+                     * 遍历完孤岛数+1
+                     * */
                     count++;
                 }
             }
@@ -24,14 +41,26 @@ public class MatrixIsland {
         return count;
     }
 
+    /**
+     * 深度优先遍历
+     * */
     public static void dfs(int[][] matrix, int i, int j) {
         if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) return;
+
+        /**
+         * 如果是0(水域) 或 2(元素已被遍历过)，则结束遍历
+         * */
         if (matrix[i][j] != 1) return;
 
-        // 标记已经遍历过
+        /**
+         * 标记当前的1元素(岛域)已经被遍历过
+         * */
         matrix[i][j] = 2;
 
-        // 递归遍历当前元素的四个方向
+        /**
+         * 递归遍历当前元素/岛域的四个方向
+         * 从而探索孤岛的范围
+         * */
         dfs(matrix, i - 1, j);
         dfs(matrix, i + 1, j);
         dfs(matrix, i, j - 1);
