0342c5c996df0dd9ef62895e1bb4bd22544f021f
xijun.lh <xijun.lh@antfin.com>
2021-01-23 21:32:04 +0800

    optimize graph using adjacency list

--- src/main/java/com/learning/java/algorithm/graph/AdjacencyList.java
+++ src/main/java/com/learning/java/algorithm/graph/AdjacencyList.java
@@ -7,7 +7,7 @@ import java.util.Map;
 /**
  * 图的数据结构实现（二）：邻接表
  * 参考：resources/photos下的adjacency_list.png
- * */
+ */
 public class AdjacencyList {
 
     static class AJGraph {
@@ -15,7 +15,7 @@ public class AdjacencyList {
         /**
          * 节点
          * 可用list来存储，也可用map来存储
-         * */
+         */
         Map<String, Vertex> vertexMap;
 
         public AJGraph(Map<String, Vertex> vertexMap) {
@@ -26,6 +26,41 @@ public class AdjacencyList {
         public void insertVertex(String vertexName) {
             Vertex vertex = new Vertex(vertexName, null);
             vertexMap.put(vertexName, vertex);
+        }
+
+        // 删除节点
+        public void deleteVertex(String vertexName) {
+            if (vertexMap.containsKey(vertexName)) {
+                vertexMap.remove(vertexName);
+                /**
+                 * 删除该节点相关的边
+                 * 该节点出发的边不用删除，因为节点已经不在vertexMap中了，不会再被遍历到了
+                 * 但到达该节点的边仍需删除，因为其他节点不知道该节点被删除了，仍会保留到该节点的边
+                 * */
+                for (String vertexKey : vertexMap.keySet()) {
+                    Vertex vertex = vertexMap.get(vertexKey);
+                    Edge currentEdge = vertex.edge;
+                    Edge preEdge = currentEdge;
+                    Edge nextEdge = currentEdge == null ? null : currentEdge.next;
+                    boolean fromHead = true;
+                    while (currentEdge != null) {
+                        if (currentEdge.vertex.equals(vertexName)) {
+                            if (fromHead) {
+                                vertex.edge = nextEdge;
+                                currentEdge = vertex.edge;
+                                preEdge = currentEdge;
+                            } else {
+                                preEdge.next = nextEdge;
+                            }
+                        } else {
+                            fromHead = false;
+                            preEdge = currentEdge;
+                        }
+                        currentEdge = nextEdge;
+                        nextEdge = nextEdge == null ? null : nextEdge.next;
+                    }
+                }
+            }
         }
 
         // 插入边
@@ -79,13 +114,13 @@ public class AdjacencyList {
         /**
          * 节点名称
          * vertexMap中的key
-         * */
+         */
         private String name;
 
         /**
          * 节点的第一条边
          * 邻接表中的边用list来存储
-         * */
+         */
         private Edge edge;
 
         public Vertex(String name, Edge edge) {
@@ -100,18 +135,18 @@ public class AdjacencyList {
          * 边的另一个尾/头节点名称
          * 若用邻接表，则此处为尾节点
          * 若用逆邻接表，则此处为头节点
-         * */
+         */
         private String vertex;
 
         /**
          * 边的权重值
-         * */
+         */
         private int weight;
 
         /**
          * 节点的下一条边
          * 邻接表中的边用list来存储
-         * */
+         */
         private Edge next;
 
         public Edge(String vertex, int weight, Edge next) {
@@ -137,17 +172,28 @@ public class AdjacencyList {
         graph.insertVertex("E");
         graph.insertVertex("F");
         // 插入7条有向边和每条边的权重
-        graph.insertEdge( "A", "B", 4);
-        graph.insertEdge( "A", "D", 5);
-        graph.insertEdge( "C", "A", 1);
-        graph.insertEdge( "D", "E", 3);
-        graph.insertEdge( "D", "F", 4);
-        graph.insertEdge( "E", "B", 2);
-        graph.insertEdge( "F", "C", 2);
+        graph.insertEdge("A", "B", 4);
+        graph.insertEdge("A", "D", 5);
+        graph.insertEdge("C", "A", 1);
+        graph.insertEdge("D", "E", 3);
+        graph.insertEdge("D", "F", 4);
+        graph.insertEdge("E", "B", 2);
+        graph.insertEdge("F", "C", 2);
 
         // 打印图中的节点和边
         graph.print();
 
+        // 删除1个节点
+        graph.deleteVertex("F");
+        System.out.println("************************");
+        System.out.println("After deleting Vertex F:");
+        graph.print();
+
+        // 再删除1个节点
+        graph.deleteVertex("D");
+        System.out.println("************************");
+        System.out.println("After deleting Vertex D:");
+        graph.print();
     }
 
 }
